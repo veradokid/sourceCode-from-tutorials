bf35de4418335afdaf274b17b1f2177d904bfe71
Johannes Bø <johannes.bo@gmail.com>
2016-08-11 14:25:20 +0200

    completed tutorial 9

--- Bucky Node.js tutorial 9/Bucky Node.js tutorial 9/app.js
+++ Bucky Node.js tutorial 9/Bucky Node.js tutorial 9/app.js
@@ -1,1 +1,6 @@
 ﻿console.log('Bucky Roberts Node.js tutorial 9 - More modules');
+
+var movies = require('./movies');
+movies.printAvatar();
+
+console.log(movies.favMovie);
---
+++ Bucky Node.js tutorial 9/Bucky Node.js tutorial 9/movies.js
@@ -0,0 +1,25 @@
+﻿//function printAvatar() {
+//    console.log("Avatar: PG-13");
+//}
+
+//function printChappie() {
+//    console.log("Chapie: R");
+//}
+
+//module.exports.avatar = printAvatar;
+//module.exports.chappie = printChappie;
+
+
+//module.exports is created behind the scene
+//first example populates this object with item/value pairs
+//following example creates the exported functions inline the exports object.
+//even variables may be exported this way. Public variables.
+module.exports = {
+    printAvatar: function () {
+        console.log("Avatar");
+    },
+    printChappie: function () {
+        console.log("Chappie");
+    },
+    favMovie: "The Matrix"
+};
