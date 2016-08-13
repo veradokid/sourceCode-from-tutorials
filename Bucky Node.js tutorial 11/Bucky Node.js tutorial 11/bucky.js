fc10af073f5aa0a0060e8cb0d0c03f08d1a81a2c
Johannes Bø <johannes.bo@gmail.com>
2016-08-13 15:23:21 +0200

    Commented out tut 10 code

--- Bucky Node.js tutorial 11/Bucky Node.js tutorial 11/bucky.js
+++ Bucky Node.js tutorial 11/Bucky Node.js tutorial 11/bucky.js
@@ -1,3 +1,4 @@
-﻿var movies = require('./movies');
-//movies.favMovies = "The Notebook";
-console.log("Bucky's favorite movie is " + movies.favMovies);
+﻿//ref tut 10
+//var movies = require('./movies');
+////movies.favMovies = "The Notebook";
+//console.log("Bucky's favorite movie is " + movies.favMovies);
--- Bucky Node.js tutorial 11/Bucky Node.js tutorial 11/movies.js
+++ Bucky Node.js tutorial 11/Bucky Node.js tutorial 11/movies.js
@@ -1,3 +1,13 @@
-﻿module.exports = {
-    favMovie:  ""
-}
+﻿// exports a property shared amongst all users (ref tut 10)
+//module.exports = {
+//    favMovie:  ""
+//}
+
+// ref tut 11 one have to export a function creating a object at the "user level". This way
+// the state is local to each user -and NOT shared between users.
+// This is called using object factory.
+module.exports = function () {
+    return {
+        favMovie: ""
+    }
+};
