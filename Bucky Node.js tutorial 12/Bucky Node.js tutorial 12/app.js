2a7fe6580766fce45e3223d1663faea7c9daf980
Johannes Bø <johannes.bo@gmail.com>
2016-08-15 12:45:07 +0200

    fs and path core modules and setInterval

--- Bucky Node.js tutorial 12/Bucky Node.js tutorial 12/app.js
+++ Bucky Node.js tutorial 12/Bucky Node.js tutorial 12/app.js
@@ -6,6 +6,27 @@
 //-Omit the .js extension
 //-name the local variable the same as the required module
 
+
+//fs (filesystem) core module
+//handling files
 var fs = require('fs'); //file system core module. Omit the .js extension
 fs.writeFileSync("corn.txt", "Corn is good for you");
 console.log(fs.readFileSync("corn.txt").toString());
+
+//Path core module
+//paths, filenames, 
+var path = require('path');
+var websiteHome = "Desktop/Bucky//thenewboston/index.html";
+var websiteAbout = "Desktop/Bucky/thenewboston/about.html"; 
+
+console.log(path.normalize(websiteHome));
+console.log(path.dirname(websiteAbout));
+console.log(path.basename(websiteAbout));
+console.log(path.extname(websiteAbout));
+
+
+//setInterval
+setInterval(function () {
+    console.log("beef");
+}, 2000);
+
