226df270ad8ebd99076140a85bde7ca14b512b3d
Johannes Bø <johannes.bo@gmail.com>
2016-08-15 12:35:54 +0200

    write and read corn.txt

--- Bucky Node.js tutorial 12/Bucky Node.js tutorial 12/app.js
+++ Bucky Node.js tutorial 12/Bucky Node.js tutorial 12/app.js
@@ -1,1 +1,11 @@
-﻿console.log('Hello world');
+﻿//Core modules
+//buildtin to Node.js
+//a ton of core modules. Will be explained as we need them
+
+//show common practise:
+//-Omit the .js extension
+//-name the local variable the same as the required module
+
+var fs = require('fs'); //file system core module. Omit the .js extension
+fs.writeFileSync("corn.txt", "Corn is good for you");
+console.log(fs.readFileSync("corn.txt").toString());
