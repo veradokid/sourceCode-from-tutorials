004f313c6562b184e0ad29969ad2d2586967ad4d
Johannes Bø <johannes.bo@gmail.com>
2016-08-15 15:08:23 +0200

    tut 14 finnished

---
+++ Bucky Node.js tutorial 14/Bucky Node.js tutorial 14/server.js
@@ -0,0 +1,23 @@
+﻿var http = require('http');
+var fs = require('fs');
+var listeningPort = 8888;
+
+//404 response
+function send404response(response) {
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.write("Error 404: Page not found!");
+    response.end();
+}
+
+//Handle a user request
+function onRequest(request, response) {
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, { "Content-Type": "text/html" });
+        fs.createReadStream("./index.html").pipe(response);
+    } else{
+        send404response(response);
+    }
+}
+
+http.createServer(onRequest).listen(listeningPort);
+console.log("Server is running, listening on port: " + listeningPort);
