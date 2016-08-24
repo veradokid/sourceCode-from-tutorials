96603c8eb94894d5506ba5fd5503e592b8dbc0e3
Johannes Bø <johannes.bo@gmail.com>
2016-08-24 13:03:56 +0200

    halfway in express tut, put jade tut in separate file

--- Bucky Node.js tutorial 18/Bucky Node.js tutorial 18/app.js
+++ Bucky Node.js tutorial 18/Bucky Node.js tutorial 18/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');    //handling cookies
 var bodyParser = require('body-parser');        //parse jason fex
 
 var routes = require('./routes/index');
+var jade_tutorial_1 = require('./routes/jade_tutorial_1');
 
 var app = express();        //init app to use express
 
@@ -22,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //all relevant urls 
 app.use('/', routes);
+app.use('/jade_tutorial_1', jade_tutorial_1);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ Bucky Node.js tutorial 18/Bucky Node.js tutorial 18/routes/jade_tutorial_1.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET jade tutorial page. */
+router.get('/', function (req, res) {
+    res.render('jade_tutorial_1', { });  //render *.jade, opt. send additional information
+});
+
+module.exports = router;
