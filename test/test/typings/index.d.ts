c7e06879a53b11b562ec04db7e2959395002a96c
Johannes Bø <johannes.bo@gmail.com>
2016-08-24 09:22:46 +0200

    cleanup

--- test/test/typings/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="globals/connect/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
--- thenewboston/tutorial-05/app.js
+++
@@ -1,3 +0,0 @@
-console.log("Node.js Tutorial for Beginners");
-console.log("5 Everything is a reference");
-
