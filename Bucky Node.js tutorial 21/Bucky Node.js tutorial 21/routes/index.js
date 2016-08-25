58392c9c7d1ea7fb6daa8de2671976757eea8f88
Johannes Bø <johannes.bo@gmail.com>
2016-08-25 11:24:21 +0200

    cleaned up index.jade and added videodata

--- Bucky Node.js tutorial 21/Bucky Node.js tutorial 21/routes/index.js
+++ Bucky Node.js tutorial 21/Bucky Node.js tutorial 21/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Johannes node tutorial examples and notes' });
 });
 
 module.exports = router;
