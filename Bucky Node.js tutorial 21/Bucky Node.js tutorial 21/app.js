0748ef5b290024672ecda3d3664df45718b1cb11
Johannes Bø <johannes.bo@gmail.com>
2016-08-25 11:35:22 +0200

    deleted users page

--- Bucky Node.js tutorial 21/Bucky Node.js tutorial 21/app.js
+++ Bucky Node.js tutorial 21/Bucky Node.js tutorial 21/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -25,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
